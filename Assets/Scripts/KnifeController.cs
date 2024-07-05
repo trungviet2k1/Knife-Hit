74aa4bac50cd64bcbca2a4cff72bc0400e8d5291
Trần Trung Việt <137788372+trungviet2k1@users.noreply.github.com>
2024-07-06 03:25:26 +0700

    Initial commit

---
+++ Assets/Scripts/KnifeController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class KnifeController : MonoBehaviour
+{
+    public static KnifeController Instance { get; private set; }
+
+    public float throwSpeed;
+    public GameObject knifePrefab;
+    public Transform knifeSpawnPoint;
+
+    private Rigidbody2D rb;
+    private bool isThrown = false;
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+        InstantiateNewKnife();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !isThrown)
+        {
+            isThrown = true;
+            rb.velocity = new Vector2(0, throwSpeed);
+        }
+    }
+
+    public void InstantiateNewKnifeAfterEmbed()
+    {
+        Invoke(nameof(InstantiateNewKnife), 0.05f);
+    }
+
+    void InstantiateNewKnife()
+    {
+        GameObject newKnife = Instantiate(knifePrefab, knifeSpawnPoint.position, Quaternion.identity, knifeSpawnPoint);
+        rb = newKnife.GetComponent<Rigidbody2D>();
+        isThrown = false;
+    }
+}
---
+++ Assets/Scripts/TargetController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class TargetController : MonoBehaviour
+{
+    public float rotationSpeed;
+    public float knifeEmbedDepth;
+    public float bounceForce = 10f;
+
+    void Update()
+    {
+        transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Knife"))
+        {
+            Rigidbody2D knifeRb = collision.gameObject.GetComponent<Rigidbody2D>();
+
+            foreach (Transform child in transform)
+            {
+                if (child.CompareTag("Knife") && child != collision.transform)
+                {
+                    if (Vector2.Distance(collision.transform.position, child.position) < 0.3f)
+                    {
+                        Debug.Log("Game Over: Knife hit another knife.");
+                        knifeRb.isKinematic = false;
+                        Vector2 bounceDirection = (collision.transform.position - child.position).normalized;
+                        knifeRb.AddForce(bounceDirection * bounceForce, ForceMode2D.Impulse);
+                        return;
+                    }
+                }
+            }
+
+            if (collision.transform.parent != transform)
+            {
+                knifeRb.velocity = Vector2.zero;
+                knifeRb.isKinematic = true;
+                collision.transform.position += collision.transform.up * knifeEmbedDepth;
+                collision.transform.SetParent(transform);
+                KnifeController.Instance.InstantiateNewKnifeAfterEmbed();
+            }
+        }
+    }
+}
